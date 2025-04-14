fc89ae99110a0c2a7e66ad56b14c49b9df279aee
iamsukanta <sukantabalacste2016@gmail.com>
2025-04-14 23:25:31 +0200

    Popup method added in linked list

--- Linked Lists/Singly Linked Lists/practice.py
+++ Linked Lists/Singly Linked Lists/practice.py
@@ -23,11 +23,32 @@ class LinkedList:
             self.length += 1
         return True
     
+    def pop(self):
+        if self.head is None:
+            print("Linked List is Empty")
+            return False
+        
+        if self.length is 1:
+            self.head = None
+            self.tail = None
+            self.length -= 1
+        else:
+            temp = self.head
+            for _ in range(self.length - 1):
+                temp = temp.next
+            self.tail = temp
+            self.tail.next = None
+            self.length -= 1
+        return True
+    
     def print_linked_list(self):
         temp_node = self.head
-        for _ in range(self.length):
-            print(temp_node.value)
-            temp_node = temp_node.next
+        if self.length:
+            for _ in range(self.length):
+                print(temp_node.value)
+                temp_node = temp_node.next
+        else:
+            print("Linked List is Empty.")
         return True
 
 my_linked_list = LinkedList(5)
@@ -36,6 +57,18 @@ my_linked_list.print_linked_list()
 
 print("----------------------------")
 
-print("After addding a new new node:")
+print("After addding a new new node at the end:")
 my_linked_list.append(87)
+my_linked_list.print_linked_list()
+
+print("After addding a new new node at the end:")
+my_linked_list.append(11)
+my_linked_list.print_linked_list()
+
+print("After pop a new new node at the end:")
+my_linked_list.pop()
+my_linked_list.print_linked_list()
+
+print("After pop a new new node at the end:")
+my_linked_list.pop()
 my_linked_list.print_linked_list()
