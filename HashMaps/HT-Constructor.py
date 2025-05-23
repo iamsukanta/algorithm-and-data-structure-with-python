4e0df81198fe5dfe23557745e876c96923d17e3c
iamsukanta <sukantabalacste2016@gmail.com>
2025-05-23 23:58:58 +0200

    Comments added

--- HashMaps/HT-Constructor.py
+++ HashMaps/HT-Constructor.py
@@ -2,12 +2,14 @@ class HashTable:
     def __init__(self, size = 7):
         self.data_map = [None]*7
     
+    # Adding Hash Methods
     def __hash(self, key):
         my_hash = 0
         for letter in key:
             my_hash = (my_hash + ord(letter) * 23)% len(self.data_map)
         return my_hash
     
+    # Set key and items
     def set_item(self, key, value):
         index = self.__hash(key)
         if self.data_map[index] == None:
