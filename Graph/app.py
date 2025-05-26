a655e787217d218a61e969dc0ce2743c7cdf3ac3
iamsukanta <sukantabalacste2016@gmail.com>
2025-05-27 00:32:43 +0200

    Remove vertex added

--- Graph/app.py
+++ Graph/app.py
@@ -19,8 +19,51 @@ class Graph:
             return True
         return False
 
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list and v2 in self.adj_list:
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+    
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list:
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
+    
 my_graph = Graph()
 my_graph.add_vertex('A')
 my_graph.add_vertex('B')
-my_graph.add_edge('A', 'B')
+my_graph.add_vertex('C')
+my_graph.add_vertex('D')
+
+my_graph.add_edge('A','B')
+my_graph.add_edge('A','C')
+my_graph.add_edge('A','D')
+my_graph.add_edge('B','D')
+my_graph.add_edge('C','D')
+
+
+print('Graph before remove_vertex():')
+my_graph.print_graph()
+
+
+my_graph.remove_vertex('D')
+
+
+print('\nGraph after remove_vertex():')
+my_graph.print_graph()
+
+
+
+my_graph.remove_edge('A','C')
+
+
+print('\nGraph after remove_edge():')
 my_graph.print_graph()
